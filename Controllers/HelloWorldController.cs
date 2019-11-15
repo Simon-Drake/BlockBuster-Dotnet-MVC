59e675ad7bf98ee28a2748d8705693286919e387
Simon <s.drake03294@gmail.com>
2019-11-15 15:04:22 +0000

    adding view

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BlockBuster_Dotnet_MVC.Controllers
+
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
