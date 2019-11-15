ce3215b1c96bda668ff46ff577a8efa9db6fc6cd
Simon <s.drake03294@gmail.com>
2019-11-15 14:08:15 +0000

    initialising web app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlockBuster_Dotnet_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
